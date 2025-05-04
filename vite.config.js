a2efdde6f71bc99e10137d2b0c7f0aa6d9d6ebd5
Sohaib Abbasi <98388413+sohaibabbasiwork@users.noreply.github.com>
2025-05-05 00:19:08 +0500

    setup vite for deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist',  // Ensure this matches the directory in wrangler.toml
+    rollupOptions: {
+      input: 'index.html',  // Ensure this is the entry point for your app
+    }
+  },
+  server: {
+    open: true,
+  },
   plugins: [react()],
 })
