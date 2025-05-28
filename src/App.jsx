50429b6eb2d745e657cea4a90ab05fc4860eb281
Sohaib Abbasi <98388413+sohaibabbasiwork@users.noreply.github.com>
2025-05-28 19:50:39 +0500

    #route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import AllProductsPage from './AllProductsPage';
 import ThankYou from './ThankYou';
