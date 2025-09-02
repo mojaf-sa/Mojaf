0a3cae9162e6c0925a1c97b07085942fce77c6e2
mojaf-sa <mojafsa@gmail.com>
2025-09-02 17:37:31 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './style.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
