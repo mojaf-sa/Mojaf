44505ce3ca7c8867e129a5b538efdca9a3d95390
mojaf-sa <mojafsa@gmail.com>
2025-09-02 21:04:29 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
-import App from "./App.jsx";
-import "./style.css";
+// src/main.jsx
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { HashRouter as Router } from 'react-router-dom';
+import App from './App.jsx';
+import './style.css';
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
+    <Router>
       <App />
-    </HashRouter>
+    </Router>
   </React.StrictMode>
 );
