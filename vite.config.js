7cdf8c2eca7a128baf926aba790ddce099c66622
mojaf-sa <mojafsa@gmail.com>
2025-09-17 02:19:14 +0500

    fix update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '', // relative asset paths (alternative)
+  base: '', // relative paths to avoid CDN/domain path issues
 })
