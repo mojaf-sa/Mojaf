cd8bb00caeeb8b6c6629e06aa053d7b61919a88c
mojaf-sa <mojafsa@gmail.com>
2025-09-17 13:49:18 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './style.css'   // ensure CSS is bundled
+import App from './App.jsx';
+import './style.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename=="/"><App /></BrowserRouter>
-
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
