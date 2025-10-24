ea3254698e83af1ea16a760fe86dd6404b6587dd
mojaf-sa <mojafsa@gmail.com>
2025-10-25 01:06:13 +0500

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// IMPORTANT: base must be '/' for a custom domain on GitHub Pages
-export default defineConfig({
-  plugins: [react()],
-  base: '/', // <-- do not use './' or '/Mojaf/'
-})
