97dbee6e0f76cd994abfeb80a72b58c7e04f5ea9
mojaf-sa <mojafsa@gmail.com>
2025-10-25 13:24:45 +0500

    Add SEO pack: canonical+hreflang, robots/sitemap, SPA 404, EN/AR toggle assets

--- src/components/Footer.jsx
+++
@@ -1,5 +0,0 @@
-import { useI18n } from '../i18n'
-export default function Footer(){
-  const { t } = useI18n()
-  return <footer style={{padding:'30px 20px', textAlign:'center', borderTop:'1px solid #e5e7eb', marginTop:40}}>{t.footer}</footer>
-}
