3e3d8ca1f916c81fa7604094b08379830d47e638
mojaf-sa <mojafsa@gmail.com>
2025-11-03 13:41:59 +0500

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// IMPORTANT: custom domain at the root should use base: '/'
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/',             // << fix broken asset URLs
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-  },
+  base: '/', 
 })
